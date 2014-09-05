050ff34057c2003c9ffa81cbb755691cd9fd440e
popomore <sakura9515@gmail.com>
2014-09-05 17:36:02 +0800

    extract server

--- example1/server.js
+++ example1/server.js
@@ -1,40 +1,14 @@
-var fs = require('fs');
 var join = require('path').join;
-var http = require('http');
-var through = require('through2');
 var url = require('url');
 var qs = require('querystring');
+var createServer = require('../lib/server');
 var he = require('he');
 
-http.createServer(function (req, res) {
-  res.writeHead(200, {
-    'Content-Type': 'text/html'
-  });
-
+createServer(function(req, res) {
   var u = url.parse(req.url);
   var query = qs.parse(u.query);
-  var model = {
+  res.render(join(__dirname, 'index.html'), {
     // a: he.encode(query.a || '') // 转义防止 xss
     a: query.a
-  };
-  render('index.html', model)
-  .pipe(res);
-}).listen(3000, '127.0.0.1');
-
-console.log('Server running at http://127.0.0.1:3000/');
-
-function render(file, model) {
-  var html = '';
-  return fs.createReadStream(join(__dirname, file))
-  .pipe(through(function transform(buf, enc, cb) {
-    html += buf;
-    cb();
-  }, function flush(cb) {
-    html = html.replace(/{{(.*?)}}/, function(a, b) {
-      return model[b] || '';
-    });
-    this.push(html);
-    html = '';
-    cb();
-  }));
-}
+  });
+});
